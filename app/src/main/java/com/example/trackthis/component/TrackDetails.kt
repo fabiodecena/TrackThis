0ae7e862baadaa5bb92e9699001154f085209a8d
fabio <fabiodecena@gmail.com>
2024-11-11 18:42:53 +0100

    11/11/24

--- app/src/main/java/com/example/trackthis/component/TrackDetails.kt
+++ app/src/main/java/com/example/trackthis/component/TrackDetails.kt
@@ -1,10 +1,14 @@
 package com.example.trackthis.component
 
+import android.icu.text.SimpleDateFormat
 import androidx.annotation.StringRes
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -12,15 +16,18 @@ import androidx.compose.material.icons.filled.AccessTime
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.AddComment
 import androidx.compose.material.icons.filled.Book
-import androidx.compose.material.icons.filled.CalendarMonth
-import androidx.compose.material.icons.filled.CalendarToday
 import androidx.compose.material.icons.filled.CheckCircleOutline
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -28,6 +35,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.dimensionResource
@@ -35,13 +43,20 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
 import com.example.trackthis.R
 import com.example.trackthis.data.Topic
 import com.example.trackthis.data.listOfVisualizedTopics
+import java.util.Date
+import java.util.Locale
 
 
 @Composable
-fun TrackDetails(modifier: Modifier = Modifier, topic: Topic) {
+fun TrackDetails(
+    modifier: Modifier = Modifier,
+    topic: Topic
+) {
     var studyNameInput by remember { mutableStateOf("") }
     var descriptionInput by remember { mutableStateOf("") }
     var dailyEffortInput by remember { mutableStateOf("") }
@@ -113,33 +128,15 @@ fun TrackDetails(modifier: Modifier = Modifier, topic: Topic) {
             value = finalGoalInput,
             onValueChanged = { finalGoalInput = it }
         )
-        EditField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(dimensionResource(R.dimen.padding_medium)),
-            label = R.string.starting_date,
-            leadingIcon = Icons.Filled.CalendarToday,
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Next
-            ),
-            value = startingDateInput,
-            onValueChanged = { startingDateInput = it }
-        )
-        EditField(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(dimensionResource(R.dimen.padding_medium)),
-            label = R.string.ending_date,
-            leadingIcon = Icons.Filled.CalendarMonth,
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType = KeyboardType.Number,
-                imeAction = ImeAction.Done
-            ),
-            value = endingDateInput,
-            onValueChanged = { endingDateInput = it }
-        )
 
+        DatePickerDocked(
+            onDateSelected = { startingDateInput = it },
+            label = R.string.starting_date
+        )
+        DatePickerDocked(
+            onDateSelected = { endingDateInput = it },
+            label = R.string.ending_date
+        )
         FloatingActionButton(
             onClick = { /*TODO*/ },
             modifier = Modifier
@@ -183,6 +180,72 @@ fun EditField(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerDocked(
+    modifier: Modifier = Modifier,
+    @StringRes label: Int,
+    onDateSelected: (String) -> Unit
+) {
+    var showDatePicker by remember { mutableStateOf(false) }
+    val datePickerState = rememberDatePickerState()
+    val selectedDate = datePickerState.selectedDateMillis?.let {
+        convertMillisToDate(it)
+    } ?: ""
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .clickable { showDatePicker = !showDatePicker } // Toggle expansion on click
+    ) {
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            value = selectedDate,
+            onValueChange = { onDateSelected },
+            label = { Text(stringResource(label)) },
+            shape = MaterialTheme.shapes.large,
+            readOnly = true,
+            colors = TextFieldDefaults.colors(
+                focusedLeadingIconColor = MaterialTheme.colorScheme.primary,
+                unfocusedLeadingIconColor = MaterialTheme.colorScheme.primary,
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent
+            ),
+            leadingIcon = {
+                IconButton(onClick = { showDatePicker = !showDatePicker }) {
+                    Icon(
+                        imageVector = Icons.Default.DateRange,
+                        contentDescription = "Select date"
+                    )
+                } },
+        )
+    }
+    if (showDatePicker) {
+        Popup(
+            onDismissRequest = { showDatePicker = false },
+            alignment = Alignment.TopStart
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .offset(y = 64.dp)
+                    .shadow(elevation = 4.dp)
+                    .background(MaterialTheme.colorScheme.surface)
+                    .padding(16.dp)
+            ) {
+                DatePicker(
+                    state = datePickerState,
+                    showModeToggle = false
+                )
+            }
+        }
+    }
+}
+fun convertMillisToDate(millis: Long): String {
+    val formatter = SimpleDateFormat("MM/dd/yyyy", Locale.getDefault())
+    return formatter.format(Date(millis))
+}
 
 
 @Preview
