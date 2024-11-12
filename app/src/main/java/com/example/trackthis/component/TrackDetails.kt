4f9ff9b6b379be49ed7b7587d97f00fb417559ed
fabio <fabiodecena@gmail.com>
2024-11-12 14:01:16 +0100

    12/11/24

--- app/src/main/java/com/example/trackthis/component/TrackDetails.kt
+++ app/src/main/java/com/example/trackthis/component/TrackDetails.kt
@@ -3,17 +3,13 @@ package com.example.trackthis.component
 import android.icu.text.SimpleDateFormat
 import androidx.annotation.StringRes
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.waitForUpOrCancellation
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
@@ -30,9 +26,7 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -45,7 +39,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.pointer.PointerEventPass
@@ -55,8 +48,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.window.Popup
 import com.example.trackthis.R
 import com.example.trackthis.data.Topic
 import com.example.trackthis.data.listOfVisualizedTopics
@@ -146,7 +137,7 @@ fun TrackDetails(
                 .fillMaxWidth()
                 .padding(dimensionResource(R.dimen.padding_medium)),
             label = R.string.ending_date,
-            onValueChanged = { startingDateInput = it }
+            onValueChanged = { endingDateInput = it }
         )
         FloatingActionButton(
             onClick = { /*TODO*/ },
