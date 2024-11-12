3fa92edcab74d93aab700a22179dca1adb015be8
fabio <fabiodecena@gmail.com>
2024-11-12 23:20:51 +0100

    12/11/24

--- app/src/main/java/com/example/trackthis/component/StartedTopic.kt
+++ app/src/main/java/com/example/trackthis/component/StartedTopic.kt
@@ -1,18 +1,54 @@
 package com.example.trackthis.component
 
-import androidx.compose.foundation.layout.Column
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.trackthis.R
 import com.example.trackthis.data.StartedTopicElement
 
 @Composable
 fun StartedTopic(
     topicElement: StartedTopicElement,
+    modifier: Modifier = Modifier
 ) {
-    Column {
-        Text(
-            text = stringResource(id = topicElement.name)
-        )
+    Card(
+        modifier = modifier
+            .padding(dimensionResource(R.dimen.padding_medium)),
+        shape = MaterialTheme.shapes.large
+    ) {
+        Row(
+            modifier = modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                modifier = Modifier.padding(dimensionResource(R.dimen.padding_medium)),
+                text = stringResource(topicElement.name),
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center
+            )
+        }
+        Row() {
+            Card(
+                modifier = Modifier
+                    .padding(dimensionResource(R.dimen.padding_medium))
+                    .height(200.dp),
+            ){
+
+            }
+        }
     }
 }
