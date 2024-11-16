c25618abc3eaf52151926e32640c3fe051de0dda
fabio <fabiodecena@gmail.com>
2024-11-16 14:27:22 +0100

    16/11/24

--- app/src/main/java/com/example/trackthis/ui/TimerUI.kt
+++ app/src/main/java/com/example/trackthis/ui/TimerUI.kt
@@ -15,7 +15,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.trackthis.component.charts.ChartViewModel
 
 
 @Composable
