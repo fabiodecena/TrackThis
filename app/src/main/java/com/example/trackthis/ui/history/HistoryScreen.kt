2a2f96ef79c2fed0fc9f030b409dc17d90491a95
fabio <fabiodecena@gmail.com>
2024-12-02 19:41:31 +0100

    02/12/24

--- app/src/main/java/com/example/trackthis/ui/history/HistoryScreen.kt
+++ app/src/main/java/com/example/trackthis/ui/history/HistoryScreen.kt
@@ -1,19 +1,36 @@
 package com.example.trackthis.ui.history
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults.buttonColors
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import com.example.trackthis.R
 import com.example.trackthis.data.database.TrackedTopic
 import com.example.trackthis.ui.insert_track.TrackEntryViewModel
@@ -42,7 +59,6 @@ fun HistoryScreen(
     }
 }
 
-
 @Composable
 fun HistoryElement(
     modifier: Modifier = Modifier,
@@ -55,8 +71,9 @@ fun HistoryElement(
             .fillMaxWidth()
             .padding(dimensionResource(R.dimen.padding_small))
     ){
-        Text("Name: ${trackedTopic.name}")
+        Text("Name: ${stringResource(trackedTopic.name)}")
         Text("Total Time Spent: ${trackedTopic.timeSpent}")
+        Text("Final Goal: ${trackedTopic.finalGoal}")
         Text("Starting Date: ${trackedTopic.startingDate}")
         Text("Ending Date: ${trackedTopic.endingDate}")
         Button(
@@ -67,6 +84,64 @@ fun HistoryElement(
             }
         ){
             Text("Delete")
+        }
+        ShowProgress((trackedTopic.timeSpent)*100/trackedTopic.finalGoal)
+    }
+}
+
+@Composable
+fun ShowProgress(score : Int){
+    val gradient = Brush.linearGradient(listOf(
+        MaterialTheme.colorScheme.primary,
+        MaterialTheme.colorScheme.secondary)
+    )
+    val progressFactor by remember(score) { mutableFloatStateOf(score*0.01f) }
+
+    Row(
+        modifier = Modifier
+        .padding(8.dp)
+        .fillMaxWidth().height(45.dp).border(
+            width = 4.dp,
+            brush = Brush.linearGradient(
+                colors = listOf(
+                    MaterialTheme.colorScheme.primary,
+                    MaterialTheme.colorScheme.secondary
+                )
+            ) ,
+            shape = RoundedCornerShape(50.dp)
+        )
+        .clip(
+            RoundedCornerShape(
+                topStartPercent = 50,
+                topEndPercent = 50,
+                bottomEndPercent = 50,
+                bottomStartPercent = 50
+            )
+        )
+        .background(Color.Transparent),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Button(
+            contentPadding = PaddingValues(1.dp),
+            onClick = { },
+            modifier = Modifier
+                .fillMaxWidth(progressFactor)
+                .background(brush = gradient),
+            enabled = false,
+            elevation = null,
+            colors = buttonColors(
+                containerColor = Color.Transparent,
+                disabledContainerColor = Color.Transparent
+            )
+        ) {
+            Text(text = (score).toString()+"%",
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(23.dp))
+                    .fillMaxHeight(0.87f)
+                    .fillMaxWidth()
+                    .padding(7.dp),
+                color = Color.White,
+                textAlign = TextAlign.Center)
         }
     }
 }
